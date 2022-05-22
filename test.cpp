39b98293971944148e96b3ec2609f56075416c7e
Ouzr0107 <zhuo0107@foxmail.com>
2022-05-22 19:01:54 +0800

    123

--- test.cpp
+++ test.cpp
@@ -1,49 +1,66 @@
 #include <iostream>
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
+struct Bridge
+{
+    int a, b, t;
+    Bridge(){};
+    Bridge(int a, int b, int t) : a(a), b(b), t(t){};
+};
+
+int find(int x, int pre[])
+{
+    if (x == pre[x])
+    {
+        return x;
+    }
+    return find(pre[x], pre);
+}
+
+bool unite(int a, int b, int* pre)
+{
+    if (find(a, pre) == find(b, pre))
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool cmp(Bridge a, Bridge b)
+{
+    return a.t > b.t;
+}
+
 int main()
 {
-    int n;
-    set<int> list;
-    set<int>::iterator it;
-    list.insert(1);
-    for (it = list.begin(); it != list.end(); ++it)
+    int n, m, a, b, t;
+    while (cin >> n >> m)
     {
-        if (list.count(859963392))
+        int pre[n], ans = 0, lastday = 0;
+        Bridge bridge[m];
+        for (int i = 0; i <= n; i++)
         {
-            break;
+            pre[i] = i;
         }
-        list.insert(*it * 2);
-        list.insert(*it * 3);
-        list.insert(*it * 5);
-    }
-    while (cin >> n && n != -1)
-    {
-        it = list.begin();
-        for (int i = 0; i < n - 1; ++i)
+        for (int i = 0; i < m; i++)
         {
-            ++it;
+            cin >> a >> b >> t;
+            bridge[i] = Bridge(a, b, t);
         }
-        string suffix;
-        if (n % 10 == 1 && (n / 10) % 10 != 1)
+        sort(bridge, bridge + m, cmp);
+        for (int i = 0; i < m; i++)
         {
-            suffix = "st";
+            if (unite(bridge[i].a, bridge[i].b, pre) && lastday != bridge[i].t)
+            {
+                ++ans;
+                lastday = bridge[i].t;
+            }
         }
-        else if (n % 10 == 2 && (n / 10) % 10 != 1)
-        {
-            suffix = "nd";
-        }
-        else if (n % 10 == 3 && (n / 10) % 10 != 1)
-        {
-            suffix = "rd";
-        }
-        else
-        {
-            suffix = "th";
-        }
-        cout << "The " << n << suffix << " ugly number is " << *it << "." << endl;
+        cout << lastday + 1 << " " << ans << endl;
     }
     return 0;
 }
