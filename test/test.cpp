3325e4a3f3005dc91c08e682d5e9c8167b64f354
Ouzr0107 <87957461+Ouzr0107@users.noreply.github.com>
2022-05-30 21:58:49 +0800

    sss

--- test/test.cpp
+++ test/test.cpp
@@ -1,21 +1,68 @@
 #include <iostream>
+#include <algorithm>
+#include <set>
+#define ll long long
 
 using namespace std;
+
+set<Result> qc;
+
+void dfs(int step, int n, Result result, ll visit[], ll num[])
+{
+    if (step == n - 1)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (visit[i] == 0)
+            {
+                result.x[step] = num[i];
+                if (!qc.count(result))
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if (j == 0)
+                        {
+                            cout << result.x[j];
+                        }
+                        else
+                        {
+                            cout << " " << result.x[j];
+                        }
+                    }
+                    qc.insert(result);
+                    cout << endl;
+                }
+            }
+        }
+        return;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (visit[i] == 0)
+        {
+            visit[i] = 1;
+            result.x[step] = num[i];
+            dfs(step + 1, n, result, visit, num);
+            visit[i] = 0;
+        }
+    }
+}
+
 int main()
 {
     int n;
     while (cin >> n)
     {
-        int x, min;
-        cin >> min;
-        for (int i = 0; i < n - 1; i++)
+        qc.clear();
+        ll num[n], visit[n];
+        Result result;
+        for (int i = 0; i < n; i++)
         {
-            if (cin >> x && x < min)
-            {
-                min = x;
-            }
+            cin >> num[i];
+            visit[i] = 0;
         }
-        cout << min << endl;
+        sort(num, num + n);
+        dfs(0, n, result, visit, num);
     }
 
     return 0;
